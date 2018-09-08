f2bbb0a807c7ce096cf77e9e1160075bb7bdd68f
root <lptnii@qq.com>
2018-09-08 22:59:06 +0800

    增加redis 模块项目

---
+++ pro-redis-service/src/main/java/com/wzy/redis/service/RedisServiceStart.java
@@ -0,0 +1,16 @@
+package com.wzy.redis.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+public class RedisServiceStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisServiceStart.class, args);
+	}
+}
