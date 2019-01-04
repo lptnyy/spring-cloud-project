4a0b9a35f336fa1d1c1ee05887b61cb3ab6ca5af
root <lptnii@qq.com>
2019-01-04 14:30:46 +0800

    增加 admin监控配置

---
+++ pro-config-service/src/main/java/com/wzy/config/ConfigStart.java
@@ -0,0 +1,16 @@
+package com.wzy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigStart.class, args);
+    }
+}
--- pro-user-service-web/src/main/java/com/wzy/user/web/controller/UserWebController.java
+++ pro-user-service-web/src/main/java/com/wzy/user/web/controller/UserWebController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserWebController {
 
-    @Autowired
+       @Autowired
     UserService userService;
 
     @Autowired
