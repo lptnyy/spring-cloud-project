9fa764bb30f1f85a5fce73bb57e1d5b4b26009ab
root <lptnii@qq.com>
2020-05-04 13:05:26 +0800

    解决zipkin不显示数据

--- pro-config/src/main/java/com/wzy/config/ConfigApplication.java
+++ pro-config/src/main/java/com/wzy/config/ConfigApplication.java
@@ -1,5 +1,4 @@
 package com.wzy.config;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
